09f55ccbaf1ccbdc8a981c0bb45ae4681ae3392a
Jake Faulkner <jakefaulkn@gmail.com>
2016-03-24 07:55:43 +1300

    Update opcodes and move them to opcode function tables

--- lib/include/opcodes.h
+++ lib/include/opcodes.h
@@ -1,6 +1,22 @@
 #ifndef OPCODES_H
 #define OPCODES_H
+#include <stdio.h>
 
+#define OPCODE_FUNC(NAME) reg NAME(int car, int cadr, int caddr)
 
+typedef struct reg {
+  int val;
+  char* debug; // for printing the interpreted results
+}reg;
 
+OPCODE_FUNC(add);
+OPCODE_FUNC(mul);
+OPCODE_FUNC(div);
+OPCODE_FUNC(sub);
+OPCODE_FUNC(ld);
+OPCODE_FUNC(sav);
+OPCODE_FUNC(p);
+OPCODE_FUNC(sto);
+reg (*FUNC_TABLE[8])(int, int, int) = {add, mul, div, sub, ld, sav, p, sto}; // Function table
+/* FUNC_TABLE functions are callable like (*FUNC_TABLE[0])(reg1, reg2)  for adding one register to another */
 #endif /* OPCODES_H */
--- lib/src/opcodes.c
+++ lib/src/opcodes.c
@@ -1,1 +1,18 @@
 #include "opcodes.h"
+// Op codes for DSL
+typedef enum {ADD, MUL, DIV, SUB, LD, SAV, P, STO} OPCODE;
+// Memory table, is just a list of integers
+int MEMTABLE[64];
+// Register struct, a wrap around an int with a debug message for the results of the last instruction
+typedef struct reg {
+  int val;
+  char* debug; // for printing the interpreted results
+}reg;
+reg REGISTERTABLE[2]; // the two registers
+// forgive my lisp
+// this macro defines an opcode function for consistency reasons
+OPCODE_FUNC(add) {
+  printf("%i", car);
+}
+const OPCODE_FUNC_PTR(FUNC_TABLE) = {add, mul, div, sub, ld, sav, p, sto};
+// TODO maybe function pointer table instead of these opcodes

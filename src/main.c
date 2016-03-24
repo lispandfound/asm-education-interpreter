c17a5ae5678e64ed953404f314e95e8e01c3ebfe
Jake Faulkner <jakefaulkn@gmail.com>
2016-03-24 18:31:10 +1300

    Add a preliminary cli interface
    This will be the fall back interface for cross platform
    reasons (requires no external libraries)

--- src/main.c
+++ src/main.c
@@ -1,5 +1,24 @@
+#include <opcodes.h>
 #include <stdio.h>
+/* This is the cross platform cli client to the system */
 int main(int argc, char *argv[])
 {
+  init_asm_interpreter();
+  while (1) {
+    char *func, *car, *cadr, *caddr;
+    func = malloc(4);
+    car = malloc(4);
+    cadr = malloc(4);
+    caddr = malloc(4);
+    printf("> ");
+    scanf("%s %s %s %s", func, car, cadr, caddr);
+    reg* res = parse_and_run(func, car, cadr, caddr);
+    puts(res->debug);
+    free(func);
+    free(car);
+    free(cadr);
+    free(caddr);
+  }
+  free_asm_interpreter();
   return 0;
 }

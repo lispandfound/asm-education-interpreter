ebbe1a9c561a297fd3b4d515658bf69fc46f26f6
Jake Faulkner <jakefaulkn@gmail.com>
2016-05-12 18:54:53 +1200

    Update documentation for opcodes.c and opcodes.h

--- lib/include/opcodes.h
+++ lib/include/opcodes.h
@@ -30,30 +30,57 @@ typedef struct reg {
   uint8_t val;
   /* Debug contains the variable for debug information (so the return result of an OPCODE, e.g ADD adding 1 + 1 together might return "-> 2") */
   char debug[255];
+  /* err is a flag that tells the CLI whether or not an error occurred */
   int err;
+  /* the actual error string buffer */
   char error[64];
 }reg;
+/* This is the memory table, it needs to be globally available as it is used across all functions */
 reg* MEMTABLE[MEMTABLE_SIZE];
+/* This is the register table, it needs to be globally available as it is used across all functions */
 reg* REGISTERTABLE[REGISTERTABLE_SIZE]; // the two registers
+/*
+  Because C does not have generics or exceptions, I do not have the ability for multiple return types of a function or indeed the ability to throw an error.
+  Therefore a register will need to be returned from the opcodes regardless of whether it succeeded or not. I am not really comfortable editing the registers in the register table,
+  these registers are for results of successful opcodes, rather than errors or io output (in p_str's case). As such I have declared two special registers, whose sole job is for error and io respectively.
+  They are not returned in any case bar error's or io output, and therefore I avoid accidentally editing the real registers.
+ */
 reg* err_reg;
 reg* io_reg;
+
+/* Opcode functions. These functions actually perform mutations on the registers. The actual function signatures can be seen at OPCODE_FUNC */
+/* Add two registers */
 OPCODE_FUNC(add);
+/* Multiple two registers */
 OPCODE_FUNC(mul);
+/* Divide two registers (div was already taken) */
 OPCODE_FUNC(divide);
+/* Subtract two registers */
 OPCODE_FUNC(sub);
+/* Load register from memory location (destructive!) */
 OPCODE_FUNC(ld);
+/* Save register to memory location (non destructive) */
 OPCODE_FUNC(sav);
+/* Print the value of a register (not to be confused with p_str, which prints a string) */
 OPCODE_FUNC(p);
+/* Store a register in memory (destructive!) */
 OPCODE_FUNC(sto);
+/* Set the value of a register */
 OPCODE_FUNC(set);
+/* Print a range of registers in memory, interpreting each register's as an ascii character (e.g 98 -> 'a') */
 OPCODE_FUNC(p_str);
+/* Check if an index is in bounds or the register table */
 int in_bounds(int i);
+/* Create a new register (allocates memory on the heap for it) */
 reg* new_register();
+/* Initialize and free the interpreter */
 void init_asm_interpreter();
 void free_asm_interpreter();
+/* Convert a character to an integer (literally '0' -> 0) */
 int ctoi(char c);
+/* Convert binary to decimal, accounting for a factor if a half byte is already taken */
 int parse_binary(char* binary, int factor);
+/* parse and run the given sections returning the results of the corresponding opcodes running back */
 reg* parse_and_run(char* func, char* car, char* cadr, char* caddr);
 void reverse_string(char * str);
-/* FUNC_TABLE functions are callable like (*FUNC_TABLE[0])(reg1, reg2)  for adding one register to another */
 #endif /* OPCODES_H */
--- lib/src/opcodes.c
+++ lib/src/opcodes.c
@@ -1,11 +1,21 @@
 #include <opcodes.h>
 
+/* FUNC_TABLE functions are callable like (*FUNC_TABLE[0])(reg1, reg2)  for adding one register to another */
 reg* (*FUNC_TABLE[FUNC_TABLE_SIZE])(int, int, int) = {add, mul, divide, sub, ld, sav, p, sto, set, p_str}; // Function table
 
 int in_bounds(int i) {
   return i >= 0 && i < REGISTERTABLE_SIZE;
 }
+
+/*
+  For a breakdown of every opcode function listed here, included example usage, side effects and known bugs, see the Documentation.org file.
+ */
+
+/*
+  Print a string, as interpreted by a range of characters between a lower limit (register 1) and an upper limit (register 2).
+ */
 OPCODE_FUNC(p_str) {
+  /* Check both the first and second arguments are in bounds */
   if (!in_bounds(car) || !in_bounds(cadr)) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Index Out Of Bounds");
@@ -14,6 +24,7 @@ OPCODE_FUNC(p_str) {
   reg* r1 = REGISTERTABLE[car];
   reg* r2 = REGISTERTABLE[cadr];
   int size = r2->val - r1->val;
+  /* Check the user hasn't got an invalid string range */
   if (size <= 0) {
     err_reg->err = INVALID_STR_LENGTH_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Invalid String Length");
@@ -21,26 +32,43 @@ OPCODE_FUNC(p_str) {
   }
   char string[size + 1];
   memset(string, 0, size + 1);
+  /* For each register in between the value of register 1 and the value of register 2 */
   for (int i = r1->val; i < r2->val; i++) {
+    /* Add to the string the character value at register one */
     reg* r = MEMTABLE[i];
     string[i] = (char)r->val;
   }
   string[size + 1] = '\0';
+  /* We use the io register instead of an actual register here because modifying the other registers could result in unexpected behaviours we can't for see. */
   sprintf(io_reg->debug, "-> %s", string);
   return io_reg;
 }
+/* Set the value of a register */
 OPCODE_FUNC(set) {
+  /* Check the first argument is in bounds */
   if (!in_bounds(car)) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Index Out Of Bounds");
     return err_reg;
   }
   reg* to = REGISTERTABLE[car];
+  /*
+    Set the value as the second plus the third arguments.
+
+    This looks like this in the interpreter
+
+    1000 0000             1111                                  1111
+         ^^^^             ^^^^                                  ^^^^
+         register to set. second argument (interpreted as 240). Third argument (interpreted as 15)
+    So the value to set becomes 240 + 15 (255).
+   */
   to->val = cadr + caddr;
   sprintf(to->debug, "-> %i", to->val);
   return to;
 }
+/* Add two registers values together */
 OPCODE_FUNC(add) {
+  /* Check both specified registers are in bounds */
   if(!in_bounds(car) || !in_bounds(cadr)) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Index Out Of Bounds");
@@ -48,11 +76,14 @@ OPCODE_FUNC(add) {
   }
   reg* to = REGISTERTABLE[car];
   reg* from = REGISTERTABLE[cadr];
+  /* add the value of the second register to the first register, storing the value in the first register. */
   to->val += from->val;
   sprintf(to->debug, "-> %i", to->val);
   return to;
 }
+/* Subtract the value of two registers for ellipses see the add function */
 OPCODE_FUNC(sub) {
+  /* ... */
   if(!in_bounds(car) || !in_bounds(cadr)) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Index Out Of Bounds");
@@ -60,12 +91,14 @@ OPCODE_FUNC(sub) {
   }
   reg* to = REGISTERTABLE[car];
   reg* from = REGISTERTABLE[cadr];
+  /* Subtract the value of ... */
   to->val -= from->val;
   sprintf(to->debug, "-> %i", to->val);
   return to;
 }
+/* Multiply the value of two registers */
 OPCODE_FUNC(mul) {
-
+  /* ... */
   if(!in_bounds(car) || !in_bounds(cadr)) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Index Out Of Bounds");
@@ -73,11 +106,14 @@ OPCODE_FUNC(mul) {
   }
   reg* to = REGISTERTABLE[car];
   reg* from = REGISTERTABLE[cadr];
+  /* Multiply the value of two registers ... */
   to->val *= from->val;
   sprintf(to->debug, "-> %i", to->val);
   return to;
 }
+/* Divide the value of one register by another */
 OPCODE_FUNC(divide) {
+  /* ... */
   if(!in_bounds(car) || !in_bounds(cadr)) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Index Out Of Bounds");
@@ -85,6 +121,7 @@ OPCODE_FUNC(divide) {
   }
   reg* to = REGISTERTABLE[car];
   reg* from = REGISTERTABLE[cadr];
+  /* Special divide by zero check :) */
   if (from->val == 0) {
     err_reg->err = DIV_BY_ZERO_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Cannot Divide By Zero");
@@ -94,20 +131,23 @@ OPCODE_FUNC(divide) {
   sprintf(to->debug, "-> %i", to->val);
   return to;
 }
-
+/* Load the value of the memory location specified by the second register to the location of the first */
 OPCODE_FUNC(ld) {
+  /* Check if the value is in the memory table, and in the register table */
   if(!in_bounds(car) || cadr > MEMTABLE_SIZE || cadr < 0) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Index Out Of Bounds");
     return err_reg;
   }
   reg* to = REGISTERTABLE[car];
+  /* memmove is destructive (as opposed to memcpy), and therefore the value of a register is not left behind when you move it */
   to = memmove(to, MEMTABLE[cadr], sizeof(reg));
   sprintf(to->debug, "-> SUCCESS");
   return to;
 }
-
+/* Save the value of a register in a memory location given by register two */
 OPCODE_FUNC(sav) {
+  /* ... */
   if(!in_bounds(car) || cadr > MEMTABLE_SIZE || cadr < 0) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Index Out Of Bounds");
@@ -115,12 +155,14 @@ OPCODE_FUNC(sav) {
   }
   reg* from = REGISTERTABLE[car];
   reg* to = MEMTABLE[cadr];
+  /* Non-destructively copy the from the value of register and save it to the memory location */
   memcpy(to, from, sizeof(reg));
   sprintf(from->debug, "-> SUCCESS");
   return from;
 }
-
+/* Print the value of a register */
 OPCODE_FUNC(p) {
+  /* Check the register is in bounds */
   if(!in_bounds(car)) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
     strcpy(err_reg->error, "-> ASM: Error: Index Out Of Bounds");
@@ -130,7 +172,7 @@ OPCODE_FUNC(p) {
   sprintf(regi->debug, "-> %i", regi->val);
   return regi;
 }
-
+/* Store the value of the register to the memory location of the second arguments, destructively */
 OPCODE_FUNC(sto) {
   if(!in_bounds(car) || cadr > MEMTABLE_SIZE || cadr < 0) {
     err_reg->err = OUT_OF_BOUNDS_ERROR;
@@ -140,11 +182,13 @@ OPCODE_FUNC(sto) {
   reg* from = REGISTERTABLE[car];
   reg* to = MEMTABLE[cadr];
   memcpy(to, from, sizeof(reg));
+  /* Write over the register */
   memset(from, 0, sizeof(reg));
   sprintf(from->debug, "-> SUCCESS");
   from->val = 0;
   return from;
 }
+/* Create a register, allocated the correct heap memory required */
 reg* new_register() {
   reg* ptr = malloc(sizeof(reg));
   ptr->val = 0;
@@ -153,6 +197,7 @@ reg* new_register() {
   ptr->err = 0;
   return ptr;
 }
+/* Initialize the interpreter, and fill the memory/register tables with registers manipulate */
 void init_asm_interpreter() {
   err_reg = new_register();
   io_reg = new_register();
@@ -173,13 +218,16 @@ void free_asm_interpreter() {
     free(MEMTABLE[i]);
   }
 }
+/* Convert any character literally to an integer ('0' -> 0) */
 int ctoi(char c) {
   if ('0' <= c &&  c <= '9') {
     return c - '0';
   } else {
+    /* character was not a number */
     return CTOI_ERR;
   }
 }
+/* Parse a binary string into a number */
 int parse_binary(char* binary, int factor) {
   int power = strlen(binary) - 1;
   int ret = 0;
@@ -195,6 +243,7 @@ int parse_binary(char* binary, int factor) {
   }
   return ret;
 }
+/* Parse and run a given set of strings (this is the main interface to the interpreter), and in turn return the register result of the opcode */
 reg* parse_and_run(char* func, char* car, char* cadr, char* caddr) {
   int function, c, cd, cdd;
   function = parse_binary(func, 0);
@@ -204,6 +253,7 @@ reg* parse_and_run(char* func, char* car, char* cadr, char* caddr) {
     return err_reg;
   }
   c = parse_binary(car, 0);
+  /* If the function  is set, then we need to parse the second and third numbers too. */
   cd = parse_binary(cadr, function == 8 ? 4 : 0);
   cdd = parse_binary(caddr, 0);
   if (function == BINARY_ERR ||
@@ -214,5 +264,6 @@ reg* parse_and_run(char* func, char* car, char* cadr, char* caddr) {
     strcpy(err_reg->error, "-> ASM: Error: Invalid Binary Input");
     return err_reg;
   }
+  /* This piece of voodo roughly translates to run the function (index function) in the array  with the arguments c, cd, and cdd */
   return (*FUNC_TABLE[function])(c, cd, cdd);
 }
